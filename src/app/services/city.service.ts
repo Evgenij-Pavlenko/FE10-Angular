8bc24bf6ff246158758c52d8dc5d418bc53b023d
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-09-21 19:44:32 +0200

    service and backend

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import {CityModel} from '../model/CityModel';
 import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
+  cities: CityModel[];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  getCities(): Observable<CityModel> {
-    return null;
+  getCities(): Observable<CityModel[]> {
+    return this.httpClient.get<CityModel[]>('assets/cities.json');
   }
 }
